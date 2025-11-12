f01c14413dfac6f521b98c91a191dfa10d5fea79
Victor.D <durochervictor@gmail.com>
2025-11-12 22:11:11 +0000

    Initial Commit

---
+++ LuxuryRetailApp/LuxuryRetailAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuxuryRetailAppApp.swift
+//  LuxuryRetailApp
+//
+//  Created by user279259 on 12/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct LuxuryRetailAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
